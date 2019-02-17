ceef8c6df74a9d799908010fbd5df1687ed9d19f
sharonmaswai <chepmaswai@gmail.com>
2019-02-17 22:30:48 +0300

    add edits to the javascript file

--- js/script.js
+++ js/script.js
@@ -2,18 +2,20 @@ function tri(){
   var sidex=parseInt(document.getElementById('x').value);
   var sidey=parseInt(document.getElementById('y').value);
   var sidez=parseInt(document.getElementById('z').value);
+  
 
 if (sidex+sidey>sidez && sidex+sidez>sidex && sidey+sidez>sidex){
   if (sidex===sidez && sidey===sidex & sidey===sidez) {
-    alert("Equilateral.");
+    alert("Equilateral.")
   }
-  else if (sidex!==sidey && sidez!==sidey && sidez!==sidex){
-    alert("Scalene.");
+  else if (sidex!==sidey && sidez!==sidey && sidez!==
+  sidex){
+    alert("Scalene.")
   }
  else {
-    alert("Isosceles.");
+    alert("Isosceles.")
   }
  } else{
-   alert("Is not a triangle");
+   alert("Is not a triangle")
  }
 }
